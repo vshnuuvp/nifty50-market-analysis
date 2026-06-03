48e7d52cd8c4b90dfae9cddf8083fe7a1791b534
vshnuuvp <vishnuvp017@gmail.com>
2026-06-03 12:01:50 +0530

    Update stock_analysis.py

--- stock_analysis.py
+++ stock_analysis.py
@@ -10,3 +10,62 @@ def get_stock_return(symbol, period):
     last_close = df["Close"].iloc[-1]
 
     return ((last_close - first_close) / first_close) * 100
+
+def get_stock_volatility(symbol, period):
+
+    stock = yf.Ticker(symbol)
+
+    df = stock.history(period=period)
+
+    daily_returns = df["Close"].pct_change()
+
+    volatility = daily_returns.std() * 100
+
+    return volatility
+
+
+def compare_volatility():
+
+    stocks = {
+        "TCS": "TCS.NS",
+        "Infosys": "INFY.NS",
+        "Reliance": "RELIANCE.NS",
+        "HDFC Bank": "HDFCBANK.NS",
+        "ICICI Bank": "ICICIBANK.NS"
+    }
+
+    results = {}
+
+    for company, symbol in stocks.items():
+
+        volatility = get_stock_volatility(symbol, "1y")
+
+        results[company] = volatility
+
+    return results
+
+
+
+def risk_return_dashboard():
+
+    stocks = {
+        "TCS": "TCS.NS",
+        "Infosys": "INFY.NS",
+        "Reliance": "RELIANCE.NS",
+        "HDFC Bank": "HDFCBANK.NS",
+        "ICICI Bank": "ICICIBANK.NS"
+    }
+
+    results = []
+
+    for company, symbol in stocks.items():
+
+        stock_return = get_stock_return(symbol, "1y")
+
+        volatility = get_stock_volatility(symbol, "1y")
+
+        results.append(
+            (company, stock_return, volatility)
+        )
+
+    return results
